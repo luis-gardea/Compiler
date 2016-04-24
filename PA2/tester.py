82e8358e338c52171fe05418e8e31e2f83f995c0
Ryan Silva <rdsilva@stanford.edu>
2016-04-24 01:40:02 -0700

    tester

--- PA2/tester.py
+++ PA2/tester.py
@@ -9,7 +9,8 @@ dirs = os.listdir( path )
 # This would print all the files and directories
 for file in dirs:
 	if file[0] != '.':
-		command1 = "./myparser tests/" + file + " > tests/output/out" + file
-		command2 = "/usr/class/cs143/bin/lexer tests/" + file + " | /usr/class/cs143/bin/parser > tests/output/ref" + file
+		command1 = "./myparser tests/" + file + " >! tests/output/out.txt"
+		command2 = "/usr/class/cs143/bin/lexer tests/" + file + " | /usr/class/cs143/bin/parser >! tests/output/ref.txt"
 		os.system(command1)
 		os.system(command2)
+		# diff
