0af88c69df09401589db96779496b40a94c20a19
Ryan Silva <rdsilva@stanford.edu>
2016-04-26 12:36:55 -0700

    added 3 to tests/ and edited tester

--- PA2/tester.py
+++ PA2/tester.py
@@ -6,13 +6,23 @@ import os, sys
 path = "tests/"
 dirs = os.listdir( path )
 
-# This would print all the files and directories
-for file in dirs:
-	if file[0] != '.' and file != 'output' and file == '2':
-		command1 = "./myparser tests/" + file + " >& tests/output/" + file + "out"
-		command2 = "/usr/class/cs143/bin/lexer tests/" + file + " | /usr/class/cs143/bin/parser >& tests/output/" + file + "ref"
-		os.system(command1)
-		os.system(command2)
-		print file
-		diff_cmd = "diff tests/output/" + file + "out" + " tests/output/" + file + "ref"
-		os.system(diff_cmd)
+if len(sys.argv) == 2:
+	command1 = "./myparser tests/" + str(sys.argv[1]) + " >& tests/output/" + str(sys.argv[1]) + "out"
+	command2 = "/usr/class/cs143/bin/lexer tests/" + str(sys.argv[1]) + " | /usr/class/cs143/bin/parser >& tests/output/" + str(sys.argv[1]) + "ref"
+	os.system(command1)
+	os.system(command2)
+	print str(sys.argv[1])
+	diff_cmd = "diff tests/output/" + str(sys.argv[1]) + "out" + " tests/output/" + str(sys.argv[1]) + "ref"
+	os.system(diff_cmd)
+else:
+	# This would print all the files and directories
+	for file in dirs:
+		if file[0] != '.' and file != 'output' and file == '2':
+			command1 = "./myparser tests/" + file + " >& tests/output/" + file + "out"
+			command2 = "/usr/class/cs143/bin/lexer tests/" + file + " | /usr/class/cs143/bin/parser >& tests/output/" + file + "ref"
+			os.system(command1)
+			os.system(command2)
+			print file
+			diff_cmd = "diff tests/output/" + file + "out" + " tests/output/" + file + "ref"
+			os.system(diff_cmd)
+			print '\n'
