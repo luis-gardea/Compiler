ee45de05ef8a1cb7b1d047b95c7b4fd341c99614
Ryan Silva <rdsilva@stanford.edu>
2016-05-16 23:16:33 -0700

    updated tester

---
+++ PA4/tester.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+import os, sys
+
+# Open a file
+path = "tests/"
+dirs = os.listdir( path )
+
+
+
+#command1 = "/usr/class/cs143/bin/lexer good.cl | /usr/class/cs143/bin/parser | ./semant -s" 
+#command2 = "/usr/class/cs143/bin/lexer good.cl | /usr/class/cs143/bin/parser | /usr/class/cs143/bin/semant" 
+#os.system(command1)
+#print "\n"
+#os.system(command2)
+if len(sys.argv) == 2:
+	path = str(sys.argv[1])
+	filename = path[path.find('/')+1:]
+	name = filename[:filename.find('.')]
+	command1 = "/usr/class/cs143/bin/lexer " + filename + " | /usr/class/cs143/bin/parser | /usr/class/cs143/bin/semant | ./cgen >& output/" + name + "out.s"
+	command2 = "/usr/class/cs143/bin/lexer " + filename + " | /usr/class/cs143/bin/parser | /usr/class/cs143/bin/semant | /usr/class/cs143/bin/cgen >& output/" + name + "ref.s"
+	os.system(command1)
+	os.system(command2)
+	print filename
+	diff_cmd = "diff output/" + name + "out.s" + " output/" + name + "ref.s"
+	os.system(diff_cmd)
+else:
+# This would print all the files and directories
+	for file in dirs:
+		if file[0] != '.':
+			command1 = "/usr/class/cs143/bin/lexer " + file + " | /usr/class/cs143/bin/parser | ./semant >& output/" + file + "out"
+			command2 = "/usr/class/cs143/bin/lexer " + file + " | /usr/class/cs143/bin/parser | /usr/class/cs143/bin/semant >& output/" + file + "ref"
+			os.system(command1)
+			os.system(command2)
+			print file
+			diff_cmd = "diff output/" + file + "out" + " output/" + file + "ref"
+			os.system(diff_cmd)
+			print '\n'
---
+++ PA4/tests/example.cl
@@ -0,0 +1,13 @@
+
+(*  Example cool program testing as many aspects of the code generator
+    as possible.
+ *)
+
+class Main {
+  main():Int { 0 };
+};
+
+class A {
+	a : Int;
+};
+
